eea579bbd37431eb9d4aa712b9872937448e9df2
Alex <truonggiang190689@gmail.com>
2026-06-03 22:41:23 +0700

    feat: add TextReveal component to the registry

--- apps/www/registry/components/hello-world/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from "@workspace/ui/components/ui/button";
-
-export function HelloWorld() {
-  return <Button>Hello World</Button>;
-}
--- apps/www/registry/demo/components/hello-world/index.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { HelloWorld } from "@/registry/components/hello-world";
-
-export const HelloWorldDemo = () => (
-  <div className="flex items-center justify-center p-8">
-    <HelloWorld />
-  </div>
-);
