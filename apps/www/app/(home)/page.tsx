e8840fc5d703f48a8851c3e629079096d85fc1da
Alex <truonggiang190689@gmail.com>
2026-06-10 20:43:18 +0700

    revert homepage ui

--- apps/www/app/(home)/page.tsx
+++ apps/www/app/(home)/page.tsx
@@ -1,5 +1,28 @@
-import { HomeIndex } from "@/components/home/home-index";
+"use client";
+
+import { Header } from "@/components/header";
+import { HomeFaq } from "@/components/home/home-faq";
+import { HomeFooter } from "@/components/home/home-footer";
+import { HomeHeroFeatures } from "@/components/home/home-hero-features";
+import { HomeLenis } from "@/components/home/home-lenis";
+import { HomeTwoWays } from "@/components/home/home-two-ways";
+import { WhoItsFor } from "@/components/home/who-its-for";
 
 export default function HomePage() {
-  return <HomeIndex />;
+  return (
+    <>
+      <Header />
+      <HomeLenis>
+        <div className="flex min-h-full w-full flex-col justify-between">
+          <div>
+            <HomeHeroFeatures />
+            <WhoItsFor />
+            <HomeTwoWays />
+            <HomeFaq />
+          </div>
+          <HomeFooter />
+        </div>
+      </HomeLenis>
+    </>
+  );
 }
