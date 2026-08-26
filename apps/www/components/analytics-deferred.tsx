4cd7df80b794f6f7f6f7a07ae0a4e5b006c6d4e7
Alex <truonggiang190689@gmail.com>
2026-08-26 17:41:15 +0700

    chore: remove speed insights from deferred analytics

--- apps/www/components/analytics-deferred.tsx
+++ apps/www/components/analytics-deferred.tsx
@@ -7,16 +7,6 @@ const Analytics = dynamic(
   { ssr: false }
 );
 
-const SpeedInsights = dynamic(
-  () => import("@vercel/speed-insights/next").then((mod) => mod.SpeedInsights),
-  { ssr: false }
-);
-
 export function DeferredAnalytics() {
-  return (
-    <>
-      <Analytics />
-      <SpeedInsights />
-    </>
-  );
+  return <Analytics />;
 }
